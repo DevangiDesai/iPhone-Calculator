11c19049652f9e898ea04f73f409b39251e34840
Devangi <devangi@shoptimize.in>
2016-10-03 11:22:49 +0530

    Initial Commit

---
+++ calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculator
+//
+//  Created by Devangi Desai on 03/10/16.
+//  Copyright © 2016 Devangi Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  calculator
+//
+//  Created by Devangi Desai on 03/10/16.
+//  Copyright © 2016 Devangi Desai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
